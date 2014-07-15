6d0d341526e05aa50b5ec8b157aac668b8f5e724
mattias <lundmark.mattias@gmail.com>
2014-07-15 20:44:49 +0200

    Added new webapp for admin stuff

---
+++ randopedia-core/src/main/java/no/extreme/randopedia/model/migration/MigrationResult.java
@@ -0,0 +1,13 @@
+package no.extreme.randopedia.model.migration;
+
+public class MigrationResult {
+	private String result;
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+}
