f9e30bebd1642e91bb56de9a73d64f030690d09b
bjoda <bjorn@donc.se>
2015-05-07 20:40:28 +0200

    Fixed client id in tour repo

--- randopedia-server/repositories/tour-repository.js
+++ randopedia-server/repositories/tour-repository.js
@@ -5,12 +5,19 @@ var tourRepository = (function () {
 
     function getTour(tourId, callback) {
 
-        tourModel.find({ clientId: tourId }, function (err, tour) {
+        tourModel.find({ clientId: tourId }, function (err, result) {
             if (err) {
                 console.error(err);
             }
+            
             if (callback) {
-                callback(tour);
+                if(result.length === 0) {
+                    callback(null);
+                    return;
+                }
+                var tour = result[0].toObject();
+                tour.id = tour.clientId;
+                callback( {tour: tour} );
             }
         });
     }
