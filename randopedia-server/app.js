afdf3208b51460fa2f7b2503d9ae2310f27add1a
bjoda <bjorn@donc.se>
2015-06-30 22:34:07 +0200

    Set data load limit to 12MB on app (needed for image upload)

--- randopedia-server/app.js
+++ randopedia-server/app.js
@@ -23,8 +23,8 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: "12mb"}));
+app.use(bodyParser.urlencoded({ extended: false, limit: "12mb" }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
