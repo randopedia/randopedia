35574e40049449ade3725de6a6d30b63ffc1a79a
Mattias <lundmark.mattias@gmail.com>
2016-10-07 11:05:29 +0200

    Started converting tag cloud component

---
+++ randopedia-web-2/app/components/tag-cloud.js
@@ -0,0 +1,43 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tags : [],
+
+  didInsertElement() {
+    var self = this;
+    setTimeout(function () {
+      self.initjQCloud();
+    });
+  },
+  initjQCloud: function() {
+      var jqTags = [];
+
+      this.get("tags").forEach(function (tag) {
+          jqTags.push({
+              text: tag.get("name"),
+              weight: tag.get("popularity") + 5,
+              link: "/tags/" + tag.get("id")
+          });
+      });
+
+      function scaleCloud() {
+          var containerWidth = $(".tagcloud-component").width();
+          if (containerWidth)
+              $(".tagcloud-container").width(containerWidth);
+          else
+              window.setTimeout(scaleCloud, 30);
+
+          $(".tagcloud-container").jQCloud(jqTags, {
+              width: containerWidth,
+              height: 300
+          });
+      }
+
+      scaleCloud();
+
+      // todo: "live" scaling doesn't work with jqCloud. width needs to be set in some other way
+      //$Jssor$.$AddEvent(window, "load", scaleCloud);
+      //$Jssor$.$AddEvent(window, "resize", scaleCloud);
+      //$Jssor$.$AddEvent(window, "orientationchange", scaleCloud);
+  }
+});
