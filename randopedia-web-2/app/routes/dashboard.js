cf75ec83f5edb8fc5fa90f55010711c08c4b1899
Mattias <lundmark.mattias@gmail.com>
2016-10-11 20:53:04 +0200

    Added tag cloud with http mocks

--- randopedia-web-2/app/routes/dashboard.js
+++ randopedia-web-2/app/routes/dashboard.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash({
+      tags : this.get('store').findAll('tag')
+    });
+  },
+
+  setupController(controller, models) {
+    controller.set('tags', models.tags);
+  }
 });
