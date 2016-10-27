9baaa177c15cca28ac1a06fdf043cdda734f7873
Mattias <lundmark.mattias@gmail.com>
2016-10-27 11:55:27 +0200

    Added last updated component

--- randopedia-web-2/app/routes/dashboard.js
+++ randopedia-web-2/app/routes/dashboard.js
@@ -1,10 +1,12 @@
 import Ember from 'ember';
+import Fixtures from '../utils/fixtures';
 
 export default Ember.Route.extend({
   model() {
     return Ember.RSVP.hash({
       tags : this.get('store').findAll('tag'),
-      statistics : this.get('store').findRecord('stat', 1)
+      statistics : this.get('store').findRecord('stat', 1),
+      tours : this.get('store').query('tour', {status : Fixtures.TourStatus.LAST_UPDATED})
     });
   },
 
@@ -13,5 +15,6 @@ export default Ember.Route.extend({
     console.log(models.statistics);
     controller.set('tags', models.tags);
     controller.set('stats', models.statistics);
+    controller.set('tours', models.tours);
   }
 });
