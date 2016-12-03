66a914f387c1c800c2b4eb5c9d144b65738b08a3
Mattias <lundmark.mattias@gmail.com>
2016-12-03 20:12:31 +0100

    Can click on tour on map

--- randopedia-web-2/app/controllers/index.js
+++ randopedia-web-2/app/controllers/index.js
@@ -5,30 +5,16 @@ export default Ember.Controller.extend({
 
     init: function() {
         var self = this;
-        
+
         self._super();
-        self.loadTourItems();
-        
+
         //var onWindowResize = function() {
         //    self.set('isSmallScreen', window.innerWidth < 768);
         //};
-        
+
         //$(window).on('resize', onWindowResize);
         //onWindowResize();
     },
-    
-    loadTourItems: function() {
-        var self = this;
-
-        self.set('liteTours', []);
-        // todo: ...
-        //self.get('store').findQuery('tourItem', {}).then(function(tourItems) {
-        //    self.set('liteTours', tourItems);
-        //}, function(error) {
-        //    alert.showErrorMessage('Error when loading tours');
-        //});
-    },
-
     actions: {
         mapZoomChanged: function(zoomLevel) {
             this.set('currentMapZoomLevel', zoomLevel);
